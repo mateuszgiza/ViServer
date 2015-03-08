c31f07e49ce3f950474159aa35ae83910c102e93
Vahaagn <matgizq@gmail.com>
2015-03-08 14:51:14 +0100

    Create git repository

---
+++ ViServer/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ViServer
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ShowMenu();
+
+			Console.ReadKey();
+		}
+
+		static void ShowMenu()
+		{
+			ConsoleKey choice;
+
+			while ( choice != ConsoleKey.D0 ) {
+				Console.WriteLine("--------------------------");
+				Console.WriteLine("\tViServer");
+				Console.WriteLine("--------------------------");
+				Console.WriteLine("[1] Start server");
+
+				choice = Console.ReadKey().Key;
+
+				switch ( choice ) {
+					case ConsoleKey.D0:
+						Console.WriteLine("* Stopping server...");
+						break;
+					case ConsoleKey.D1:
+						CreateServer();
+						break;
+					default:
+						Console.WriteLine("* Try again!");
+						break;
+				}
+			}
+		}
+
+		static void CreateServer()
+		{
+			
+		}
+	}
+
+	class Server
+	{
+		public Server(string ip, int port, int size, string pwd = null)
+		{
+
+		}
+
+		public void Start()
+		{
+			Console.WriteLine("** Starting server...");
+		}
+	}
+}
