d9a177df6645d89ff7ab6e98b15b4b14d3a67f84
Vahaagn <matgizq@gmail.com>
2015-08-17 00:38:23 +0200

    Extracted Settings

---
+++ ViCommV2/Classes/Settings.cs
@@ -0,0 +1,210 @@
+﻿using Microsoft.Win32;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Windows.Media;
+using ViData;
+
+using Brushes = System.Windows.Media.Brushes;
+
+namespace ViCommV2.Classes
+{
+    public class Settings : ViewModelBase, IDisposable
+    {
+        #region General
+
+        // The path to the key where Windows looks for startup applications
+        private readonly RegistryKey _regStartup =
+            Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+
+        private bool _runOnStartup;
+
+        public bool RunOnStartup
+        {
+            get { return _runOnStartup; }
+            set
+            {
+                _runOnStartup = value;
+
+                if (_runOnStartup) {
+                    _regStartup.SetValue("ViComm", Tools.GetStartupPath());
+                }
+                else {
+                    _regStartup.DeleteValue("ViComm", false);
+                }
+
+                NotifyPropertyChanged();
+            }
+        }
+
+        private bool _alwaysOnTop;
+
+        public bool AlwaysOnTop
+        {
+            get { return _alwaysOnTop; }
+            set
+            {
+                _alwaysOnTop = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion General
+
+        #region Fonts
+
+        private Font _messageFont = new Font("Segoe UI", 12);
+
+        public Font MessageFont
+        {
+            get { return _messageFont; }
+            set
+            {
+                _messageFont = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private Font _dateFont = new Font("Segoe UI", 9);
+
+        public Font DateFont
+        {
+            get { return _dateFont; }
+            set
+            {
+                _dateFont = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion Fonts
+
+        #region Colors
+
+        private SolidColorBrush _messageForeground = Brushes.LightGray;
+
+        public SolidColorBrush MessageForeground
+        {
+            get { return _messageForeground; }
+            set
+            {
+                _messageForeground = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private SolidColorBrush _dateForeground = BrushExtension.FromARGB("#87000000");
+
+        public SolidColorBrush DateForeground
+        {
+            get { return _dateForeground; }
+            set
+            {
+                _dateForeground = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private SolidColorBrush _bgColor = Brushes.DarkSlateGray;
+
+        public SolidColorBrush BackgroundColor
+        {
+            get { return _bgColor; }
+            set
+            {
+                _bgColor = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private SolidColorBrush _borderColor = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF436363");
+
+        public SolidColorBrush BorderColor
+        {
+            get { return _borderColor; }
+            set
+            {
+                _borderColor = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private SolidColorBrush _rowUserColor = Brushes.CadetBlue;
+
+        public SolidColorBrush RowUserColor
+        {
+            get { return _rowUserColor; }
+            set
+            {
+                _rowUserColor = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private SolidColorBrush _rowSenderColor = Brushes.MediumSeaGreen;
+
+        public SolidColorBrush RowSenderColor
+        {
+            get { return _rowSenderColor; }
+            set
+            {
+                _rowSenderColor = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        #endregion Colors
+
+        #region Dispose Implementation
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Settings()
+        {
+            Dispose(false);
+        }
+
+        private bool _disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) {
+                return;
+            }
+
+            if (disposing) {
+                // Free any other managed objects here.
+                _messageFont.Dispose();
+                _dateFont.Dispose();
+            }
+            // Free any unmanaged objects here.
+
+            _disposed = true;
+        }
+
+        #endregion Dispose Implementation
+    }
+
+    [Serializable]
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        //make it protected, so it is accessible from Child classes
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            if (PropertyChanged != null) {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
+
+namespace System.Runtime.CompilerServices
+{
+    internal sealed class CallerMemberNameAttribute : Attribute { }
+}
---
+++ ViCommV2/Classes/SettingsProvider.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Media;
+using System.Xml;
+using ViData;
+
+using Brushes = System.Windows.Media.Brushes;
+using FontStyle = System.Drawing.FontStyle;
+
+namespace ViCommV2.Classes
+{
+    public class SettingsProvider : IDisposable
+    {
+        private static SettingsProvider _instance;
+        public string CurrentFile = Tools.GetStartupPath() + @"\settings.xml";
+        public Settings Settings;
+
+        private SettingsProvider()
+        {
+            Settings = new Settings();
+            LoadSettings();
+        }
+
+        public static SettingsProvider Instance
+        {
+            get
+            {
+                if (_instance == null) {
+                    _instance = new SettingsProvider();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void LoadDefault()
+        {
+            // General
+            Settings.RunOnStartup = false;
+            Settings.AlwaysOnTop = false;
+
+            // Fonts
+            Settings.MessageFont = new Font("Segoe UI", 12);
+            Settings.DateFont = new Font("Segoe UI", 9);
+
+            // Colors
+            Settings.MessageForeground = Brushes.LightGray;
+            Settings.DateForeground = Brushes.CadetBlue;
+            Settings.BackgroundColor = Brushes.DarkSlateGray;
+            Settings.BorderColor = (SolidColorBrush)new BrushConverter().ConvertFrom("#FF436363");
+            Settings.RowUserColor = Brushes.CadetBlue;
+            Settings.RowSenderColor = Brushes.MediumSeaGreen;
+        }
+
+        public void LoadSettings()
+        {
+            var path = CurrentFile;
+            LoadSettings(path);
+        }
+
+        public void LoadSettings(string path)
+        {
+            CurrentFile = path;
+
+            if (File.Exists(path)) {
+                ReadXml(path);
+            }
+            else {
+                SaveSettings(path);
+            }
+        }
+
+        public void SaveSettings(string path)
+        {
+            var xmlWriterSettings = new XmlWriterSettings {
+                Indent = true,
+                IndentChars = "\t",
+                NewLineOnAttributes = true
+            };
+
+            using (var writer = XmlWriter.Create(path, xmlWriterSettings)) {
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Settings");
+
+                // General
+                writer.WriteStartElement("RunOnStartup");
+                writer.WriteAttributeString("Value", Settings.RunOnStartup.ToString());
+                writer.WriteEndElement();
+
+                writer.WriteStartElement("AlwaysOnTop");
+                writer.WriteAttributeString("Value", Settings.AlwaysOnTop.ToString());
+                writer.WriteEndElement();
+
+                // Message Font
+                writer.WriteStartElement("MessageFont");
+                writer.WriteAttributeString("FontFamily", Settings.MessageFont.Name);
+                writer.WriteAttributeString("FontSize", Settings.MessageFont.Size.ToString());
+                writer.WriteAttributeString("FontStyle", Settings.MessageFont.Style.ToString());
+                writer.WriteAttributeString("MessageForeground", Settings.MessageForeground.ToARGB());
+                writer.WriteEndElement();
+
+                // Message Font
+                writer.WriteStartElement("DateFont");
+                writer.WriteAttributeString("FontFamily", Settings.DateFont.Name);
+                writer.WriteAttributeString("FontSize", Settings.DateFont.Size.ToString());
+                writer.WriteAttributeString("FontStyle", Settings.DateFont.Style.ToString());
+                writer.WriteAttributeString("DateForeground", Settings.DateForeground.ToARGB());
+                writer.WriteEndElement();
+
+                // Background Color
+                writer.WriteStartElement("BackgroundColor");
+                writer.WriteAttributeString("Value", Settings.BackgroundColor.ToARGB());
+                writer.WriteEndElement();
+
+                // Border Color
+                writer.WriteStartElement("BorderColor");
+                writer.WriteAttributeString("Value", Settings.BorderColor.ToARGB());
+                writer.WriteEndElement();
+
+                // Row User Color
+                writer.WriteStartElement("RowUserColor");
+                writer.WriteAttributeString("Value", Settings.RowUserColor.ToARGB());
+                writer.WriteEndElement();
+
+                // Row Sender Color
+                writer.WriteStartElement("RowSenderColor");
+                writer.WriteAttributeString("Value", Settings.RowSenderColor.ToARGB());
+                writer.WriteEndElement();
+
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+            }
+        }
+
+        private void ReadXml(string path)
+        {
+            bool bValue;
+            string color;
+            string foreground;
+            string fontFamily;
+            float fontSize;
+            FontStyle fontStyle;
+
+            LoadDefault();
+
+            using (var reader = XmlReader.Create(path)) {
+                while (reader.Read()) {
+                    if (!reader.IsStartElement()) {
+                        continue;
+                    }
+
+                    switch (reader.Name) {
+                        // General
+                        case "RunOnStartup":
+                            bValue = reader.GetAttribute("Value").ToBoolean() ?? false;
+                            Settings.RunOnStartup = bValue;
+                            break;
+
+                        case "AlwaysOnTop":
+                            bValue = reader.GetAttribute("Value").ToBoolean() ?? false;
+                            Settings.AlwaysOnTop = bValue;
+                            break;
+
+                        // Fonts
+                        case "MessageFont":
+                            fontFamily = reader.GetAttribute("FontFamily") ?? "Segoe UI";
+                            fontSize = reader.GetAttribute("FontSize").ToFloat() ?? 12;
+                            fontStyle =
+                                Extensions.ParseEnum<FontStyle>(reader.GetAttribute("FontStyle") ?? "Regular");
+                            foreground = reader.GetAttribute("MessageForeground") ?? "#FFD3D3D3";
+
+                            Settings.MessageFont = new Font(fontFamily, fontSize, fontStyle);
+                            Settings.MessageForeground = BrushExtension.FromARGB(foreground);
+                            break;
+
+                        case "DateFont":
+                            fontFamily = reader.GetAttribute("FontFamily") ?? "Segoe UI";
+                            fontSize = reader.GetAttribute("FontSize").ToFloat() ?? 9;
+                            fontStyle =
+                                Extensions.ParseEnum<FontStyle>(reader.GetAttribute("FontStyle") ?? "Regular");
+                            foreground = reader.GetAttribute("DateForeground") ?? "#87000000";
+
+                            Settings.DateFont = new Font(fontFamily, fontSize, fontStyle);
+                            Settings.DateForeground = BrushExtension.FromARGB(foreground);
+                            break;
+
+                        // Colors
+                        case "BackgroundColor":
+                            color = reader.GetAttribute("Value") ?? "#FF2F4F4F";
+
+                            Settings.BackgroundColor = BrushExtension.FromARGB(color);
+                            break;
+
+                        case "BorderColor":
+                            color = reader.GetAttribute("Value") ?? "#FF436363";
+
+                            Settings.BorderColor = BrushExtension.FromARGB(color);
+                            break;
+
+                        case "RowUserColor":
+                            color = reader.GetAttribute("Value") ?? "#FF5F9EA0";
+
+                            Settings.RowUserColor = BrushExtension.FromARGB(color);
+                            break;
+
+                        case "RowSenderColor":
+                            color = reader.GetAttribute("Value") ?? "#FF3CB371";
+
+                            Settings.RowSenderColor = BrushExtension.FromARGB(color);
+                            break;
+                    }
+                }
+            }
+        }
+
+        #region Dispose Implementation
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~SettingsProvider()
+        {
+            Dispose(false);
+        }
+
+        private bool _disposed;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (_disposed) {
+                return;
+            }
+
+            if (disposing) {
+                // Free any other managed objects here.
+                Settings.Dispose();
+            }
+            // Free any unmanaged objects here.
+
+            _disposed = true;
+        }
+
+        #endregion Dispose Implementation
+    }
+}
