2de5e1e063ab0200280fdfc180edfba6203d9d26
Vahaagn <matgizq@gmail.com>
2015-08-17 00:38:57 +0200

    Extracted classes from main program

---
+++ ViServer/Client.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+using ViData;
+
+namespace ViServer
+{
+    public class Client : IDisposable
+    {
+        private int _id;
+        private string _guid;
+        public string Name;
+
+        private Socket _clientSocket;
+        private Thread _thread;
+
+        public Client(int id, Socket socket)
+        {
+            _id = id;
+            _guid = Guid.NewGuid().ToString();
+            _clientSocket = socket;
+        }
+
+        public Thread SetThread
+        {
+            set { _thread = value; }
+        }
+
+        public int Id
+        {
+            get { return _id; }
+        }
+
+        public void Start(object o)
+        {
+            Receive();
+        }
+
+        public void Send(Packet p)
+        {
+            switch (p.Type) {
+                case PacketType.Login:
+                    // Reserved
+                    break;
+                case PacketType.Disconnect:
+                    // Reserved
+                    break;
+                case PacketType.Register:
+                    // Reserved
+                    break;
+                case PacketType.SingleChat:
+                    // Implemented
+                    break;
+                case PacketType.MultiChat:
+                    // Implemented
+                    break;
+            }
+        }
+
+        public void Receive()
+        {
+            byte[] buffer;
+            int readBytes;
+
+            while (_clientSocket != null && _clientSocket.Connected) {
+                try {
+                    buffer = new byte[_clientSocket.SendBufferSize];
+                    readBytes = _clientSocket.Receive(buffer);
+
+                    if (readBytes > 0) {
+                        Packet p = new Packet(buffer);
+
+                        if (p.Type != PacketType.Disconnect) {
+                            new Task(() => Received(p)).Start();
+                        }
+                        else {
+                            CloseConnection();
+                        }
+                    }
+                }
+                catch (SocketException e) {
+                    Console.WriteLine(e);
+                    CloseConnection();
+                }
+            }
+        }
+
+        public void Received(Packet p)
+        {
+            switch (p.Type) {
+                case PacketType.Login:
+                    Login(p.User);
+                    break;
+                case PacketType.Disconnect:
+                    CloseConnection();
+                    break;
+                case PacketType.Register:
+                    Register(p.User);
+                    break;
+
+                case PacketType.Information:
+                    InformationReceived(p);
+                    break;
+
+                case PacketType.SingleChat:
+                    SingleChat(p);
+                    break;
+                case PacketType.MultiChat:
+                    MultiChat(p);
+                    break;
+            }
+        }
+
+        public static void SingleChat(Packet p)
+        {
+            Client client = Server.Clients.Find(c => c.Name.Equals(p.Receiver));
+
+            client?._clientSocket.Send(p.ToBytes());
+        }
+
+        public static void MultiChat(Packet p)
+        {
+            foreach (Client c in Server.Clients) {
+                c._clientSocket.Send(p.ToBytes());
+            }
+        }
+
+        private void MultiChatWithoutMe(Packet p)
+        {
+            foreach (Client c in Server.Clients) {
+                if (c != this) {
+                    c._clientSocket.Send(p.ToBytes());
+                }
+            }
+        }
+
+        private void Login(User u)
+        {
+            bool result = u.Login();
+            string msg;
+
+            if (result) {
+                Name = u.Username;
+                Console.WriteLine("[{0}][Login] {1} with Guid: {2}", _id, Name, _guid);
+                msg = _guid;
+
+                Packet p = new Packet(PacketType.Information);
+                p.Information = new Information(InformationType.Joining, Name, "joined!");
+                MultiChatWithoutMe(p);
+            }
+            else {
+                u = null;
+                msg = "error";
+            }
+
+            Packet p1 = new Packet(PacketType.Login);
+            p1.User = u;
+            p1.Message = msg;
+            p1.Information = new Information(InformationType.Contacts, Server.GetContacts());
+
+            _clientSocket.Send(p1.ToBytes());
+        }
+
+        private void Register(User user)
+        {
+            //User user = new User(u);
+            bool result = user.Register();
+
+            Packet res = new Packet(PacketType.Register);
+            if (result) {
+                res.Message = "1You've successfully registered!";
+                res.User = user;
+            }
+            else {
+                res.Message = "0Something went wrong!";
+            }
+            _clientSocket.Send(res.ToBytes());
+        }
+
+        private void InformationReceived(Packet p)
+        {
+            if (p.Information.Type == InformationType.Writing) {
+                MultiChatWithoutMe(p);
+            }
+        }
+
+        private void CloseConnection()
+        {
+            Console.WriteLine("[{0}] User {1} disconnected!", _id, Name);
+
+            Packet p = new Packet(PacketType.Information);
+            p.Information = new Information(InformationType.Leaving, Name, "disconnected!");
+            MultiChatWithoutMe(p);
+
+            Disconnect();
+        }
+
+        private void Disconnect()
+        {
+            Dispose();
+        }
+
+        public void Dispose()
+        {
+            _clientSocket.Close();
+            _clientSocket.Dispose();
+            _clientSocket = null;
+            Server.Clients.Remove(this);
+            _thread.Abort();
+        }
+    }
+}
---
+++ ViServer/Server.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using ViData;
+
+namespace ViServer
+{
+    public class Server
+    {
+        private Socket _listenerSocket;
+        private IPEndPoint ip;
+
+        public Server()
+        {
+            this.ip = new IPEndPoint(IPAddress.Any, 5555);
+        }
+
+        public void Start()
+        {
+            //Database db = new Database(true);
+
+            Console.WriteLine("* Starting server...");
+
+            try {
+                _listenerSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                _listenerSocket.Bind(ip);
+
+                Console.WriteLine("* Listening on {0}:{1}", ip.Address.ToString(), ip.Port);
+                Listen();
+            }
+            catch (SocketException e) {
+                Console.WriteLine("|#| SocketException: {0}", e);
+            }
+
+            _listenerSocket.Close();
+        }
+
+        public void Stop()
+        {
+            Stop("Server closed!");
+        }
+
+        public void Stop(string msg)
+        {
+            Packet p = new Packet(PacketType.Disconnect, "Server", msg);
+
+            Client.MultiChat(p);
+        }
+
+        public static List<Client> Clients;
+        public static List<Thread> Threads;
+        private void Listen()
+        {
+            Clients = new List<Client>();
+            Threads = new List<Thread>();
+
+            int id = 0;
+            while (true) {
+                _listenerSocket.Listen(0);
+                Console.WriteLine("\n* Waiting for a connection...");
+
+                Client c1 = new Client(id, _listenerSocket.Accept());
+                Clients.Add(c1);
+                Console.WriteLine("^ Connected!");
+
+                Thread t = new Thread(c1.Start);
+                c1.SetThread = t;
+                t.Start();
+                //ThreadPool.QueueUserWorkItem(c1.Start);
+                id++;
+            }
+        }
+
+        public static List<string> GetContacts()
+        {
+            return Clients.Select(c => c.Name).ToList();
+        }
+    }
+}
