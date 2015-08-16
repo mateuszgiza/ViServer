68ffd5ced3851aae65a1d563b1590dec854e522b
Vahaagn <matgizq@gmail.com>
2015-08-17 00:37:04 +0200

    Extracted enums

---
+++ ViCommV2/Enums.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ViCommV2
+{
+    public enum FormState
+    {
+        Null,
+        Exit,
+        Close,
+        Logout
+    }
+
+    public enum RowType
+    {
+        Information,
+        User,
+        Sender
+    }
+}
